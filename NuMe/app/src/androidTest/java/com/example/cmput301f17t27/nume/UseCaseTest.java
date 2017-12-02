2c23bab752c1c1528443d2b1955f783636988646
Matt Kluk <spongebob1894@hotmail.com>
2017-12-02 15:24:40 -0700

    Added more intent tests

--- NuMe/app/src/androidTest/java/com/example/cmput301f17t27/nume/UseCaseTest.java
+++ NuMe/app/src/androidTest/java/com/example/cmput301f17t27/nume/UseCaseTest.java
@@ -132,11 +132,83 @@ public class UseCaseTest extends ActivityInstrumentationTestCase2<LoginActivity>
         solo.waitForText("Events");
         solo.assertCurrentActivity("Wrong Activity", ViewHabitActivity.class);
         solo.clickInList(0);
-        solo.waitForText("Comment");
+        solo.waitForText("Back");
         solo.assertCurrentActivity("Wrong Activity", ViewEventActivity.class);
         solo.clickOnButton("Delete");
         solo.waitForText("Events");
         solo.assertCurrentActivity("Wrong Activity", ViewHabitActivity.class);
+    }
+
+    public void testEditHabit() {
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+        solo.clearEditText((EditText) solo.getView(R.id.username));
+        solo.enterText((EditText) solo.getView(R.id.username), "intentTest");
+        solo.clickOnButton("Login");
+        solo.waitForText("Habits");
+        solo.assertCurrentActivity("Wrong Activity", HabitListActivity.class);
+        solo.clickOnButton("Add");
+        solo.waitForText("Add a Habit");
+        solo.assertCurrentActivity("Wrong Activity", AddHabitActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.title), "Test Habit");
+        solo.enterText((EditText) solo.getView(R.id.reason), "Test Reason");
+        solo.enterText((EditText) solo.getView(R.id.date), "02/12/2017");
+        solo.clickOnCheckBox(0);
+        solo.clickOnCheckBox(1);
+        solo.clickOnCheckBox(2);
+        solo.clickOnCheckBox(3);
+        solo.clickOnCheckBox(4);
+        solo.clickOnCheckBox(5);
+        solo.clickOnCheckBox(6);
+        solo.clickOnButton("Add");
+        solo.waitForText("Habits");
+        solo.assertCurrentActivity("Wrong Activity", HabitListActivity.class);
+        solo.clickInList(0);
+        solo.waitForText("Events");
+        solo.assertCurrentActivity("Wrong Activity", ViewHabitActivity.class);
+        solo.clickOnButton("Edit");
+        solo.waitForText("Save");
+        solo.assertCurrentActivity("Wrong Activity", EditHabitActivity.class);
+        solo.clearEditText((EditText) solo.getView(R.id.title));
+        solo.enterText((EditText) solo.getView(R.id.title), "test edit");
+        solo.clickOnButton("Save");
+        solo.waitForText("Events");
+        solo.assertCurrentActivity("Wrong Activity", ViewHabitActivity.class);
+    }
+
+    public void testEditHabitEvent() {
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+        solo.clearEditText((EditText) solo.getView(R.id.username));
+        solo.enterText((EditText) solo.getView(R.id.username), "intentTest");
+        solo.clickOnButton("Login");
+        solo.waitForText("Habits");
+        solo.assertCurrentActivity("Wrong Activity", HabitListActivity.class);
+        solo.clickInList(0);
+        solo.waitForText("Events");
+        solo.assertCurrentActivity("Wrong Activity", ViewHabitActivity.class);
+        solo.clickOnButton("Add Event");
+        solo.waitForText("Use Location");
+        solo.assertCurrentActivity("Wrong Activity", AddEventActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.comment), "testEvent");
+        solo.clickOnButton("Add");
+        solo.waitForText("Events");
+        solo.assertCurrentActivity("Wrong Activity", ViewHabitActivity.class);
+        solo.clickInList(0);
+        solo.waitForText("Back");
+        solo.assertCurrentActivity("Wrong Activity", ViewEventActivity.class);
+        solo.clickOnButton("Edit");
+        solo.waitForText("Use Location");
+        solo.assertCurrentActivity("Wrong Activity", EditEventActivity.class);
+        solo.clearEditText((EditText) solo.getView(R.id.comment));
+        solo.enterText((EditText) solo.getView(R.id.comment), "test edit");
+        solo.clickOnButton("Save");
+        solo.waitForText("Back");
+        solo.assertCurrentActivity("Wrong Activity", ViewEventActivity.class);
+        solo.clickOnButton("Back");
+        solo.waitForText("Events");
+        solo.assertCurrentActivity("Wrong Activity", ViewHabitActivity.class);
+        solo.clickOnButton("Delete");
+        solo.waitForText("Habits");
+        solo.assertCurrentActivity("Wrong Activity", HabitListActivity.class);
     }
 
     @Override
